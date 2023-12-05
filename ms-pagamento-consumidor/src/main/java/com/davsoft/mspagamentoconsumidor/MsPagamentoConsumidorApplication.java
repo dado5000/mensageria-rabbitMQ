168f6ac91c719f501abda0022e637fdb36323476
Daverson Saldanha <dds.saldanha@gmail.com>
2023-12-04 23:18:41 -0300

    Criando projeto consumidor

---
+++ ms-pagamento-consumidor/src/main/java/com/davsoft/mspagamentoconsumidor/MsPagamentoConsumidorApplication.java
@@ -0,0 +1,13 @@
+package com.davsoft.mspagamentoconsumidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPagamentoConsumidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPagamentoConsumidorApplication.class, args);
+	}
+
+}
---
+++ ms-pagamento-consumidor/src/test/java/com/davsoft/mspagamentoconsumidor/MsPagamentoConsumidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davsoft.mspagamentoconsumidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagamentoConsumidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
