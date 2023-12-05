1b9a9aad1b4a084e23866c8d9ea956958e159c62
Daverson Saldanha <dds.saldanha@gmail.com>
2023-12-04 23:07:04 -0300

    Criando projeto produtor das mensagens

---
+++ ms-pedido-produtor/src/main/java/com/davsoft/mspedidoprodutor/MsPedidoProdutorApplication.java
@@ -0,0 +1,13 @@
+package com.davsoft.mspedidoprodutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPedidoProdutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPedidoProdutorApplication.class, args);
+	}
+
+}
---
+++ ms-pedido-produtor/src/test/java/com/davsoft/mspedidoprodutor/MsPedidoProdutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davsoft.mspedidoprodutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidoProdutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
